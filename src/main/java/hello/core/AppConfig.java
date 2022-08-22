e626400b7ef7de9e3cb3a6ce0d59c85c0d47d514
hangillee <skfcb10@naver.com>
2022-08-22 23:17:23 +0900

    CHORE Edit application code to Spring Container

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,21 +1,44 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+//이것이 DI Container!
+@Configuration
 public class AppConfig {
     //Dependency Injection : 의존관계 주입 Impl 클래스들이 DIP를 준수할 수 있게 해준다.
     //생성자 주입 MemberServiceImpl은 이제 추상화(인터페이스)에만 의존한다.
+    @Bean //Spring 컨테이너에 등록됨
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     //생성자 주입 OrderServiceImpl은 이제 추상화(인터페이스)에만 의존한다.
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    //역할을 좀 더 세세하게 구분했다.
+    //중복이 제거 되었고 변경이 용이하게 되었다.
+    //이제 역할과 구현 클래스가 한 눈에 들어온다.
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
